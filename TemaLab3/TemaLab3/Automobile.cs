b8207d821a477bd6ac1e051527f6216f8116b945
Stefan Sovea <stefansovea@outlook.com>
2020-03-18 11:53:38 +0200

    Problema 1

---
+++ TemaLab3/TemaLab3/Automobile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaLab3
+{
+    class Automobile
+    {
+        string marca;
+        string culoare;
+        long pret;
+
+        public Automobile()
+        {
+            marca = string.Empty;
+            culoare = string.Empty;
+            pret = 0;
+        }
+        public Automobile(string _marca, string _culoare, long _pret)
+        {
+            marca = _marca;
+            culoare = _culoare;
+            pret = _pret;
+        }
+
+        public Automobile(string sir)
+        {    int i  = 0;
+            string[] date = sir.Split(',');
+            foreach(var cuvant in date)
+            {
+                if (i == 0)
+                    marca = cuvant;
+                if (i == 1)
+                    culoare = cuvant;
+                if (i == 2)
+                    pret = Convert.ToInt64(cuvant);
+                i++;
+            }
+            
+        }
+
+        public string afisare()
+        {
+            return string.Format(" {0}, de culoare {1}, la pretul de {2} euro \n", marca, culoare, pret);
+        }
+
+        public string getmarca()
+        {
+            return marca;
+        }
+
+        public string getculoare()
+        {
+            return culoare;
+        }
+
+        public long getpret()
+        {
+            return pret;
+        }
+
+        public string verifica(long _buget)
+        {
+            if (_buget >= pret)
+                return string.Format("Isi permite masina");
+            else
+                return string.Format("Nu isi permite masina");
+        }
+
+
+    }
+}
---
+++ TemaLab3/TemaLab3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+
+namespace TemaLab3
+{
+    //    Suma/diferenta a doua numere preluate de la tastatura
+    class Program
+    {
+        static void Main()
+        {
+            long buget;
+            Automobile audi = new Automobile("audi", "Rosu", 50000);
+            string a = audi.afisare();
+            Console.WriteLine(a);
+            Automobile bmw = new Automobile("bmw", "Albastru", 70000);
+            string b = bmw.afisare();
+            Console.WriteLine(b);
+            Automobile toyota = new Automobile("toyota", "Alb", 20000);
+            string c = toyota.afisare();
+            Automobile dacia = new Automobile("dacia,verde,10000");
+            Console.WriteLine(c);
+            string d = dacia.afisare();
+            Console.WriteLine(d);
+            Console.WriteLine("Optiunea dorita:");
+            string optiune = Console.ReadLine();
+            Console.WriteLine("Introduceti bugetul dumneavoastra:");
+            buget = Convert.ToInt64(Console.ReadLine());
+            if (optiune.Equals(audi.getmarca()))
+                Console.WriteLine(audi.verifica(buget));
+            if (optiune.Equals(bmw.getmarca()))
+                Console.WriteLine(bmw.verifica(buget));
+            if (optiune.Equals(toyota.getmarca()))
+                Console.WriteLine(toyota.verifica(buget));
+
+
+        }
+    }
+}
