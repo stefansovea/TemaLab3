3a6b656f5ed82ea047d2a737dcac500e8f44705b
Stefan Sovea <stefansovea@outlook.com>
2020-03-24 20:10:30 +0200

    Problema 4

---
+++ EvidentaStudenti3/EvidentaStudenti3/Program.cs
@@ -0,0 +1,78 @@
+﻿using LibrarieEntitati;
+using System;
+
+
+namespace ManagementStudenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[][] _note = new int[4][];
+            Student s = new Student("Ionescu");
+            // s.SetNote("1 2 3 4 5 6 7");      
+            char c;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("     MENIU    ");
+                Console.WriteLine("S: Citire note de la tastatura ca un sir de caractere");
+                Console.WriteLine("T: Citire note de la tastatura (câte o nota pe rand)");
+                Console.WriteLine("C: Citire note din linia de comanda ");
+                Console.WriteLine("A: Afisare note ");
+                Console.WriteLine("I: Info autor ");
+                Console.WriteLine("X: Iesire ");
+                c = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (c)
+                {
+                    case 'i':
+                        Console.WriteLine("Sovea Stefan, grupa 3121A");
+                        Console.ReadKey();
+                        break;
+                    case 'x':
+                        Environment.Exit(1);
+                        break;
+                    case 's':
+                        Console.WriteLine("Introduceti notele:");
+                        string note_ = s.CitireSirCaractere();
+                        s.SetNote(note_);                   
+                        Console.ReadKey();
+                        break;
+                    case 'a':
+                        Console.WriteLine(s.ConversieLaSir());
+                        int sub, peste;
+                        s.NumarareNote(out sub, out peste);
+                        Console.WriteLine("Are {0} note sub nota 5", sub);
+                        Console.WriteLine("Are {0} note peste nota 5", peste);
+                        Console.ReadKey();
+                        break;
+                    case 't':
+                        for (int i = 0; i < 4; i++)
+                        {
+                            Console.WriteLine("Introduceti numarul de note:");
+                            n = Convert.ToInt32(Console.ReadLine());
+                            _note[i] = new int[n];
+                            for (int j = 0; j < n; j++)
+                            {
+                                _note[i][j] = Convert.ToInt32(Console.ReadLine());
+                            }
+                        }
+                        s.SetNote(_note);
+                        Console.ReadKey();
+                        break;
+                    case 'c':
+                        string str = String.Join(" ", args);
+                        Console.WriteLine(str);
+                        s.SetNote(str);
+                        Console.ReadKey();
+                        break;
+
+                }
+
+            } while (1 != 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EvidentaStudenti3/EvidentaStudenti3/Student.cs
@@ -0,0 +1,112 @@
+﻿namespace LibrarieEntitati
+{
+    public class Student
+    {
+        // date membre private
+        string nume;
+        int[][] note = new int[4][];
+
+        // contructor implicit
+        public Student()
+        {
+        }
+
+        // constructor cu parametri
+        public Student(string _nume)
+        {
+            nume = _nume;
+
+        }
+
+ 
+        //citirea de la tastatura a unui string
+        public string CitireSirCaractere()
+        {
+            string _note = System.Console.ReadLine();
+            return _note;
+        }
+
+        //Separarea string-ului de note in note pe disciplina si pe ani
+        public void SetNote(string sirNote)
+        {
+            int k, intreg,n;
+            string[] _note = sirNote.Split(',');
+            string[] note_;
+            for (int i = 0; i < _note.Length; i++)
+            {
+                k = 0;
+                n=0;
+                note_ = _note[i].Split(' ');
+                foreach (string arg in note_)
+                {
+                    if (int.TryParse(arg, out intreg))
+                    {
+                        n++;
+                    }
+                }
+                note[i] = new int[n];
+                foreach (string arg in note_)
+                {
+                    if (int.TryParse(arg, out intreg))
+                    {
+                        note[i][k] = intreg;
+                        k++;
+                    }
+                }
+            }
+        }
+
+        //Metoda SetNote
+        public void SetNote(int[][] _note)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                note[i] = new int [_note[i].Length];
+                for (int j = 0; j < _note[i].Length; j++)
+                {
+                    note[i][j] = _note[i][j];
+                }
+            }
+        }
+
+
+        //Metoda de numarare a notelor peste 5 si sub 5, ignorand spatiile necompletate din matrice
+        public void NumarareNote(out int valSub5, out int valPeste5)
+        {
+            valSub5 = valPeste5 = 0;
+            int linie = 4, coloana;
+            for (int i = 0; i < linie; i++)
+            {
+                coloana = note[i].Length;
+                for (int j = 0; j < coloana; j++)
+                {
+                    if (note[i][j] >= 5 && note[i][j] != 0)
+                        valPeste5++;
+                    if (note[i][j] < 5 && note[i][j] != 0)
+                        valSub5++;
+                }
+            }
+
+        }
+
+        //Metoda de afisare
+        public string ConversieLaSir()
+        {
+            string sNote = "";
+            if (note != null)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    sNote = sNote + "anul " + (i+1) + ": ";
+                    for (int j = 0; j < note[i].Length; j++)
+                    {
+                        sNote = sNote + " " + note[i][j];
+                    }
+                    sNote = sNote + "\n";
+                }
+            }
+            string s = string.Format("Studentul {0} are notele:.... \n{1}", nume, sNote);
+            return s;
+        }
+    }
+}
