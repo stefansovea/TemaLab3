cfe8afb02e2214edb6da67d7afbd596c30c2cb29
Stefan Sovea <stefansovea@outlook.com>
2020-03-25 12:51:03 +0200

    Problema 1 (update)

--- Problema1/TemaLab3/Automobile.cs
+++ Problema1/TemaLab3/Automobile.cs
@@ -12,40 +12,6 @@ namespace TemaLab3
         string culoare;
         long pret;
 
-        public Automobile()
-        {
-            marca = string.Empty;
-            culoare = string.Empty;
-            pret = 0;
-        }
-        public Automobile(string _marca, string _culoare, long _pret)
-        {
-            marca = _marca;
-            culoare = _culoare;
-            pret = _pret;
-        }
-
-        public Automobile(string sir)
-        {    int i  = 0;
-            string[] date = sir.Split(',');
-            foreach(var cuvant in date)
-            {
-                if (i == 0)
-                    marca = cuvant;
-                if (i == 1)
-                    culoare = cuvant;
-                if (i == 2)
-                    pret = Convert.ToInt64(cuvant);
-                i++;
-            }
-            
-        }
-
-        public string afisare()
-        {
-            return string.Format(" {0}, de culoare {1}, la pretul de {2} euro \n", marca, culoare, pret);
-        }
-
         public string getmarca()
         {
             return marca;
@@ -61,13 +27,81 @@ namespace TemaLab3
             return pret;
         }
 
-        public string verifica(long _buget)
+        public Automobile()
         {
-            if (_buget >= pret)
-                return string.Format("Isi permite masina");
-            else
-                return string.Format("Nu isi permite masina");
+            marca = string.Empty;
+            culoare = string.Empty;
+            pret = 0;
         }
+        public Automobile(string _marca, string _culoare, long _pret)
+        {
+            marca = _marca;
+            culoare = _culoare;
+            pret = _pret;
+        }
+
+        public Automobile(string sir)
+        {
+            int i = 0;
+            string[] date = sir.Split(',');
+            foreach (var cuvant in date)
+            {
+                if (i == 0)
+                    marca = cuvant;
+                if (i == 1)
+                    culoare = cuvant;
+                if (i == 2)
+                    pret = Convert.ToInt64(cuvant);
+                i++;
+            }
+
+        }
+
+        public int Preferinte(string optiune, string opcul, long buget)
+        {
+
+            if (optiune.Equals(marca))
+            {
+                if (opcul.Equals(culoare))
+                {
+                    if (buget >= pret)
+                        return 1;
+                    else
+                        return 2;
+                }
+            }
+            return 0;
+        }
+
+        public int verifCuloare(string _culoare)
+        {
+            if (_culoare.Equals(culoare))
+                return 1;
+            else
+                return 0;
+        }
+
+        public int verifMarca(string _marca)
+        {
+            if (_marca.Equals(marca))
+                return 1;
+            else
+                return 0;
+        }
+
+        public int verifPret(long buget)
+        {
+            if (buget >= pret)
+                return 1;
+            else
+                return 0;
+        }
+
+        public string afisare()
+        {
+            return string.Format(" {0}, de culoare {1}, la pretul de {2} euro \n", marca, culoare, pret);
+        }
+
 
 
     }
--- Problema1/TemaLab3/Program.cs
+++ Problema1/TemaLab3/Program.cs
@@ -3,36 +3,137 @@ using System.Configuration;
 
 namespace TemaLab3
 {
-    //    Suma/diferenta a doua numere preluate de la tastatura
     class Program
     {
         static void Main()
         {
+            int x, q, b;
             long buget;
-            Automobile audi = new Automobile("audi", "Rosu", 50000);
-            string a = audi.afisare();
-            Console.WriteLine(a);
-            Automobile bmw = new Automobile("bmw", "Albastru", 70000);
-            string b = bmw.afisare();
-            Console.WriteLine(b);
-            Automobile toyota = new Automobile("toyota", "Alb", 20000);
-            string c = toyota.afisare();
+            Automobile[] masini = new Automobile[4];
+            Automobile audi = new Automobile("audi", "rosu", 50000);
+            Automobile bmw = new Automobile("bmw", "albastru", 70000);
+            Automobile toyota = new Automobile("toyota", "alb", 20000);
             Automobile dacia = new Automobile("dacia,verde,10000");
-            Console.WriteLine(c);
-            string d = dacia.afisare();
-            Console.WriteLine(d);
-            Console.WriteLine("Optiunea dorita:");
-            string optiune = Console.ReadLine();
-            Console.WriteLine("Introduceti bugetul dumneavoastra:");
-            buget = Convert.ToInt64(Console.ReadLine());
-            if (optiune.Equals(audi.getmarca()))
-                Console.WriteLine(audi.verifica(buget));
-            if (optiune.Equals(bmw.getmarca()))
-                Console.WriteLine(bmw.verifica(buget));
-            if (optiune.Equals(toyota.getmarca()))
-                Console.WriteLine(toyota.verifica(buget));
+            masini[0] = audi;
+            masini[1] = bmw;
+            masini[2] = toyota;
+            masini[3] = dacia;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("     MENIU    ");
+                Console.WriteLine("A: Afisati modelele disponibile:");
+                Console.WriteLine("P: Verificare masina in functie de preferinte");
+                Console.WriteLine("C: Optiuni in functie de Culoare  ");
+                Console.WriteLine("M: Optiuni in functie de Marca ");
+                Console.WriteLine("B: Optiuni in functie de Buget");
+                Console.WriteLine("I: Info autor ");
+                Console.WriteLine("X: Iesire ");
+                x = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (x)
+                {
+                    case 'i':
+                        Console.WriteLine("Sovea Stefan, grupa 3121A");
+                        Console.ReadKey();
+                        break;
+                    case 'x':
+                        Environment.Exit(1);
+                        break;
+                    case 'a':
+                        Console.WriteLine("Optiuni disponibile:");
+                        Console.WriteLine();
+                        for (int j = 0; j < 4; j++)
+                        {
+                            string c = masini[j].afisare();
+                            Console.Write(c);
+                        }
+                        Console.ReadKey();
+                        break;
+                    case 'p':
+                        Console.WriteLine("Marca dorita:");
+                        string optiune = Console.ReadLine();
+                        Console.WriteLine("Culoarea dorita:");
+                        string opcul = Console.ReadLine();
+                        Console.WriteLine("Introduceti bugetul dumneavoastra:");
+                        buget = Convert.ToInt64(Console.ReadLine());
+                        q = 0;
+                        for (int j = 0; j < 4; j++)
+                        {
+                            q = masini[j].Preferinte(optiune, opcul, buget);
+                            if (q == 1)
+                            {
+                                Console.WriteLine("Optiunea exista si va permiteti sa o achizitionati");
+                                break;
+                            }
+                            if (q == 2)
+                            {
+                                Console.WriteLine("Optiunea exista, dar nu va permiteti sa o achizitionati");
+                                break;
+                            }
+                        }
+                        if (q == 0)
+                            Console.WriteLine("Optiunea nu exista ");
+                        Console.ReadKey();
+                        break;
+                    case 'c':
+                        Console.WriteLine("Introduceti culoarea cautata:");
+                        string cul = Console.ReadLine();
+                        b = 0;
+                        for (int t = 0; t < 4; t++)
+                        {
+                            b = 0;
+                            b = masini[t].verifCuloare(cul);
+                            if (b == 1)
+                            {
+                                Console.WriteLine(masini[t].afisare());
+                                break;
+                            }
+                        }
+                        if (b == 0)
+                            Console.WriteLine("Nu sunt optiuni disponibile");
+                        Console.ReadKey();
+                        break;
+                    case 'm':
+                        Console.WriteLine("Introduceti marca cautata:");
+                        string mar = Console.ReadLine();
+                        b = 0;
+                        for (int t = 0; t < 4; t++)
+                        {
+                            b = 0;
+                            b = masini[t].verifMarca(mar);
+                            if (b == 1)
+                            {
+                                Console.WriteLine(masini[t].afisare());
+                                break;
+                            }
 
+                        }
+                        if (b == 0)
+                            Console.WriteLine("Nu sunt optiuni disponibile");
 
+                        Console.ReadKey();
+                        break;
+                    case 'b':
+                        Console.WriteLine("Introduceti bugetul de care dispuneti:");
+                        long bug = Convert.ToInt64(Console.ReadLine());
+                        b = 0;
+                        for (int t = 0; t < 4; t++)
+                        {
+                            b = 0;
+                            b = masini[t].verifPret(bug);
+                            if (b == 1)
+                                Console.WriteLine(masini[t].afisare());
+                        }
+                        if (b == 0)
+                            Console.WriteLine("Nu sunt optiuni disponibile");
+                        Console.ReadKey();
+                        break;
+
+                }
+
+            } while (1 != 0);
+            Console.ReadKey();
         }
     }
 }
